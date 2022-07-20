8689895e98e298f5b36ed0ffb1e359af9037da65
GitHub Action <action@github.com>
2022-07-20 08:47:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/boheastill/pd2/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/boheastill/pd2/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.boheastill.pd2.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.boheastill.pd2.MyBundle
 
 class MyApplicationService {
 
--- src/test/kotlin/com/github/boheastill/pd2/MyPluginTest.kt
+++ src/test/kotlin/com/github/boheastill/pd2/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.boheastill.pd2
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.psi.xml.XmlFile
